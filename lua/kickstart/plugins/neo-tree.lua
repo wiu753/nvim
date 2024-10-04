cf838599ae4f0fcf8cabf8bd11530524f4231292
Martin Wiu <martinwiu@proton.me>
2024-10-04 13:12:19 +0200

    Update init, lazy-lock, and neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    { '|', ':Neotree reveal right<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
       window = {
         mappings = {
-          ['\\'] = 'close_window',
+          ['|'] = 'close_window',
         },
       },
     },
